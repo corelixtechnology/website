7ef391ab9c745bf3030bbeb005453a4540d6f971
Corelix Technology <corelixtechnology@gmail.com>
2026-07-06 11:53:42 +0530

    feat: remove project budget estimator section from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,31 +3,22 @@ import Hero from '../components/Hero';
 import TrustBrands from '../components/TrustBrands';
 import ServicesIntro from '../components/ServicesIntro';
 import TechStack from '../components/TechStack';
-import CoffeeCalculator from '../components/CoffeeCalculator';
 import ContactForm from '../components/ContactForm';
 
 export default function Home() {
-  const handleScrollToCalculator = () => {
-    const calcElement = document.querySelector('.calculator-pane');
-    if (calcElement) {
-      calcElement.scrollIntoView({ behavior: 'smooth' });
-      // Shake effect
-      calcElement.classList.add('calc-shake-effect');
-      setTimeout(() => {
-        calcElement.classList.remove('calc-shake-effect');
-      }, 800);
+  const handleScrollToContact = () => {
+    const contactElement = document.querySelector('#contact');
+    if (contactElement) {
+      contactElement.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
     <div>
-      <Hero onStartCalculator={handleScrollToCalculator} />
+      <Hero onStartCalculator={handleScrollToContact} />
       <TrustBrands />
       <ServicesIntro />
       <TechStack />
-      <div id="estimator">
-        <CoffeeCalculator />
-      </div>
       <ContactForm />
     </div>
   );
