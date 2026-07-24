82762e31fea6ef52e993cde614550a40b22306da
Corelix Technology <corelixtechnology@gmail.com>
2026-07-24 16:03:33 +0530

    feat: initialize React root with BrowserRouter in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
